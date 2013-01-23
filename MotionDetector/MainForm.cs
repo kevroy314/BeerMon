48d727d889ebb07ea1233a7a56607e74e0621cc7
kevroy314 <kevin.horecka@gmail.com>
2013-01-23 15:59:16 -0600

    Added Display
    -Adjusted the time and magnitude output to match up with easy-to-parse
    D3 input.
    -Added D3 Line example to read (consider changing to area chart
    http://bl.ocks.org/3883195 )
    
    TODO:
    Add WebSockets or other method for read-time updating when file updates
    (see
    http://stackoverflow.com/questions/14490037/reload-tsv-file-without-refreshing-page
    )
    
    Possible leads:
    http://mbostock.github.com/d3/tutorial/bar-2.html
    http://www.tutorialspoint.com/html5/html5_websocket.htm
    
    http://stackoverflow.com/questions/10200910/create-hello-wold-websocket-example
    
    Other possible additions:
    http://bl.ocks.org/3884955
    https://github.com/mbostock/d3/wiki/Gallery

--- MotionDetector/MainForm.cs
+++ MotionDetector/MainForm.cs
@@ -259,12 +259,76 @@ namespace MotionDetectorSample
 
                     if (fileLogWriter != null && motionLevel != 0)
                     {
-                        fileLogWriter.WriteLine("t = " + DateTime.Now.ToBinary() + " : x = " + motionLevel);
+                        DateTime n = DateTime.Now;
+                        fileLogWriter.WriteLine(n.ToString("MMddyyyyHHmmssfff") + "\t" + ToLongString(motionLevel));
                     }
                 }
             }
         }
+        private static string ToLongString(double input)
+        {
+            string str = input.ToString().ToUpper();
 
+            // if string representation was collapsed from scientific notation, just return it:
+            if (!str.Contains("E")) return str;
+
+            bool negativeNumber = false;
+
+            if (str[0] == '-')
+            {
+                str = str.Remove(0, 1);
+                negativeNumber = true;
+            }
+
+            string sep = Thread.CurrentThread.CurrentCulture.NumberFormat.NumberDecimalSeparator;
+            char decSeparator = sep.ToCharArray()[0];
+
+            string[] exponentParts = str.Split('E');
+            string[] decimalParts = exponentParts[0].Split(decSeparator);
+
+            // fix missing decimal point:
+            if (decimalParts.Length == 1) decimalParts = new string[] { exponentParts[0], "0" };
+
+            int exponentValue = int.Parse(exponentParts[1]);
+
+            string newNumber = decimalParts[0] + decimalParts[1];
+
+            string result;
+
+            if (exponentValue > 0)
+            {
+                result =
+                    newNumber +
+                    GetZeros(exponentValue - decimalParts[1].Length);
+            }
+            else // negative exponent
+            {
+                result =
+                    "0" +
+                    decSeparator +
+                    GetZeros(exponentValue + decimalParts[0].Length) +
+                    newNumber;
+
+                result = result.TrimEnd('0');
+            }
+
+            if (negativeNumber)
+                result = "-" + result;
+
+            return result;
+        }
+
+        private static string GetZeros(int zeroCount)
+        {
+            if (zeroCount < 0)
+                zeroCount = Math.Abs(zeroCount);
+
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < zeroCount; i++) sb.Append("0");
+
+            return sb.ToString();
+        }
         // Update some UI elements
         private void Application_Idle( object sender, EventArgs e )
         {
@@ -566,6 +630,7 @@ namespace MotionDetectorSample
                 {
                     fileLogWriter = new StreamWriter(diag.FileName);
                     fileLogWriter.AutoFlush = true;
+                    fileLogWriter.WriteLine("time\tmotionlevel");
                 }
 
                 ((ToolStripMenuItem)sender).Text = "Stop Logging Motion To File";
